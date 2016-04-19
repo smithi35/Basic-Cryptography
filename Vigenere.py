41b8258404af4dcc488b7c31a6456f50c80912ee
Ian Smith <ian_smith_12a@mymts.net>
2016-04-19 10:21:47 -0500

    Found the problem with decrypting multiple lines using Vigenere, the key must be restart for each line

--- Vigenere.py
+++ Vigenere.py
@@ -2,6 +2,8 @@ from Letter import Letter
 import fileinput
 import sys
 
+A = 65
+
 def main() :
 	op = sys.argv[1]
 	filename = sys.argv[2]
@@ -39,9 +41,6 @@ def get_alphabets(key) :
 def encrypt(filename, key, alphabets) :
 	contents = get_contents(filename)
 
-	# first create a string of the key repeating over and over
-	length = len(contents)
-	
 	repeating = ""
 	index = 0
 	for i in range(len(contents)) :
@@ -52,47 +51,94 @@ def encrypt(filename, key, alphabets) :
 			index = index + 1
 		else :
 			repeating = repeating + char
-	
-	# now compare the two texts and create the encrypted text
+
 	output = ""
 	count = 0
 	for i in range(len(contents)) :
-		char = contents[i]
+		target_char = contents[i]
 		
-		if (Letter.isLetter(char)) :
-			current_alphabet = alphabets[count]
-			# using alphabets[count] find the letter at char's position
-			char_value = ord(char)
-			char_value = char_value - 65
-			# print(char_value)
-			output = output + current_alphabet[char_value]
+		if (Letter.isLetter(target_char)) :
+			target_value = Letter(target_char).get_value() -1
+			repeat_char = repeating[i]
+			repeat_value = Letter(repeat_char).get_value() -1
+			shift = ((target_value + repeat_value) % 26) + 65
+			out = chr(shift)
 			
+			output = output + out
 			count = count + 1
 			if count > len(alphabets)-1 :
 				count = 0
 		else :
-			output = output + char
+			output = output + target_char
 	
 	return output
+	
+def caesar_cypher(letter, mod) :
+	value = letter.get_value()
+	value = value + mod
+	
+	if (value < 1) :
+		value = value + 26
+	elif (value > 26) :
+		value = value % 26
+		value = value + 1
+	
+	value = value + Letter.bottom_number
+	return chr(value-1)
+
 
 def decrypt(filename, key, alphabets) :
 	contents = get_contents(filename)
 	
+	# split contents by newline characters?
+	lines = contents.split("\n")
+	print(lines)
+	exit()
+	
 	output = ""
+	count = 0
 	for char in contents:
 		if (Letter.isLetter(char)):
-			curr = Letter(char)
-			cyphered = caesar_cypher(curr, mod)
-			output = output + cyphered
+			current_alphabet = alphabets[count]
+			
+			repeat_value = find_in_alphabet(char, current_alphabet)
+			
+			char_value = 65 + repeat_value
+			output = output + chr(char_value)
+			
+			count = count + 1
+			if count > len(alphabets) -1:
+				count = 0
 		else:
 			output = output + char
 
+	return output
+
+def find_in_alphabet(letter, alphabet) :
+	out = len(alphabet)
+	for i in range(len(alphabet)) :
+		if alphabet[i] is letter :
+			out = i
+	
+	return out
+
+def get_mod(alphabet) :
+	mod = 0 + ord(alphabet[0]) - 65
+	return mod
+			
 def get_contents(filename) :
 	file = open(filename, "r")
 	contents = file.read()
 	contents = contents.upper()
 	
 	return contents
+
+# given a string of multiple lines, return an array containing the lines with content
+def get_lines(string) :
+	output = string.split("\n")
+	
+	# remove lines that are just the empty string
+	
 
 if len(sys.argv) is 4 :
 	main()
