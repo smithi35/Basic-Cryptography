732ab853db7fe429a16bea41b7c422756b6cd9d9
Ian Smith <ian_smith_12a@mymts.net>
2016-05-29 21:56:58 -0500

    started writing the A1Z26 cipher application

---
+++ Cryptography/A1Z26/A1Z26.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A1Z26
+{
+    class A1Z26
+    {
+        public string Input { get; set; }
+
+        public string Encode()
+        {
+            return "";
+        }
+
+        public string Decode()
+        {
+            return "";
+        }
+    }
+}
